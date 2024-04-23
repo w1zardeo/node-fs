f9703008fa5e7e51930e7a8db04e97e8a8bf334c
Andrii Plodnik <w1zardeot@gmail.com>
2024-04-23 12:31:54 +0300

    Update 1

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('fs/promises');
+const path = require('path');
 
 const sumNumbers = async (fileA, fileB) => {
     try {
-        const dataA = await fs.readFile(fileA, 'utf-8');
-        const dataB = await fs.readFile(fileB, 'utf-8');
+        const dataA = await fs.readFile(path.resolve(fileA), 'utf-8');
+        const dataB = await fs.readFile(path.resolve(fileB), 'utf-8');
 
         const numA = parseInt(dataA);
         const numB = parseInt(dataB);
@@ -15,4 +16,7 @@ const sumNumbers = async (fileA, fileB) => {
     }
 }
 
-sumNumbers('./data/a.txt', './data/b.txt');
+const pathDataA = './data/a.txt';
+const pathDataB = './data/b.txt';
+
+sumNumbers(pathDataA, pathDataB);
