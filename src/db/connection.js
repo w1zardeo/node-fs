3293e222cc930586390d24564e3db30e26e1f7e7
Andrii Plodnik <w1zardeot@gmail.com>
2024-04-26 15:54:20 +0300

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const MONGO_DB_CONNECTION_STRING = process.env.MONGO_ATLAS_CONNECTION_STRING || '';
-console.log("1",MONGO_DB_CONNECTION_STRING);
 const connectDb = async () => {
     try {
         await mongoose.connect(MONGO_DB_CONNECTION_STRING);
